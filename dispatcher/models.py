d2b5cb0047d389ac2894786870dec941f474425b
Tamir Mandreev <tamirmandreev@mail.ru>
2025-03-18 15:59:22 +0800

    Создал модель Recipient (Получатель рассылки)

--- dispatcher/models.py
+++ dispatcher/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+# 1. Управление клиентами
+class Recipient(models.Model):
+    '''
+    Модель получателя рассылки
+
+    Представляет клиента (получателя рассылки). Содержит email,
+    Ф.И.О., комментарий
+    '''
+
+    email = models.EmailField(unique=True, verbose_name='Адрес электронной почты')
+    full_name = models.CharField(max_length=200, verbose_name='ФИО')
+    comment = models.TextField(verbose_name='Комментарий')
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Получатель рассылки'
+        verbose_name_plural = 'Получатели рассылки'
+        ordering = ['email']
+        db_table = 'Получатели рассылки'
+
+
+
