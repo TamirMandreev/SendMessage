439c72b4cc994a988979ff64592e84ab912be972
Tamir Mandreev <tamirmandreev@mail.ru>
2025-03-18 16:05:56 +0800

    Создал маршрут, который обрабатывает представление RecipientListView

--- dispatcher/urls.py
+++ dispatcher/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
+from dispatcher import views
 from dispatcher.apps import DispatcherConfig
 
 # Задать имя приложения в пространстве имен (namespace)
 app_name = DispatcherConfig.name
 
 urlpatterns = [
-
+    path('', views.RecipientListView.as_view(), name='recipients_list'),
 ]
