b34bd281d2cb42b7cdf74ee9138f2dadfdd22670
Tamir Mandreev <tamirmandreev@mail.ru>
2025-03-18 16:05:32 +0800

    Создал представление RecipientListView

--- dispatcher/views.py
+++ dispatcher/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from dispatcher.models import Recipient
+
 
 # Create your views here.
+# 1. Управление клиентами
+
+class RecipientListView(ListView):
+    '''
+    Представление для отображения всех получателей рассылки (клиентов)
+    '''
+    model = Recipient
+    template_name = 'dispatcher/recipients_list.html'
+    context_object_name = 'recipients'
