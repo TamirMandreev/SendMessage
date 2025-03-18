7732baded41fd6cb6b86909145dfaafffde48bfa
Tamir Mandreev <tamirmandreev@mail.ru>
2025-03-18 13:15:32 +0800

    Создал файл маршрутов в приложении dispatcher

---
+++ dispatcher/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from dispatcher.apps import DispatcherConfig
+
+# Задать имя приложения в пространстве имен (namespace)
+app_name = DispatcherConfig.name
+
+urlpatterns = [
+
+]
