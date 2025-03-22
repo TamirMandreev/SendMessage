c5167daa50dba1ed77dd7da424aea32c8b8a359b
Tamir Mandreev <tamirmandreev@mail.ru>
2025-03-23 00:52:21 +0800

    Привел в порядок отображение моделей в формах и админке

--- dispatcher/models.py
+++ dispatcher/models.py
@@ -35,6 +35,16 @@ class Message(models.Model):
     theme = models.CharField(max_length=200, verbose_name='Тема')
     body = models.TextField(verbose_name='Сообщение')
 
+    def __str__(self):
+        return self.theme
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+        db_table = 'Сообщения'
+
+
+
 # 3. Управление рассылками
 class Mailing(models.Model):
     '''
@@ -47,8 +57,8 @@ class Mailing(models.Model):
     LAUNCHED = '2' # Рассылка активна и была отправлена хотя бы один раз
     COMPLETED = '3' # Время окончания отправки рассылки прошло
 
-    date_time_of_first_mailing = models.DateTimeField(null=True, blank=True)
-    date_time_end_mailing = models.DateField(null=True, blank=True)
+    date_time_of_first_mailing = models.DateTimeField(null=True, blank=True, verbose_name='Дата и время первой отправки')
+    date_time_end_mailing = models.DateField(null=True, blank=True, verbose_name='Дата и время окончания отправки')
 
     STATUS_CHOICES = [
         (CREATED, 'Создана'),
@@ -57,7 +67,16 @@ class Mailing(models.Model):
     ]
     status = models.CharField(max_length=6, choices=STATUS_CHOICES, default=CREATED, verbose_name='Статус')
 
-    message = models.ForeignKey(Message, on_delete=models.SET_NULL, related_name='mailings', null=True, blank=True)
+    message = models.ForeignKey(Message, on_delete=models.SET_NULL, related_name='mailings', null=True, blank=True, verbose_name='Сообщение')
     recipients = models.ManyToManyField(Recipient, related_name='recipients')
+
+    def get_recipients(self):
+        return ', '.join([recipient.email for recipient in self.recipients.all()])
+
+    class Meta:
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
+        db_table = 'Рассылки'
+
 
 
